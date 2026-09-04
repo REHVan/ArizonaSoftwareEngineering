b1e67dc921daa07630ac0c7ad4d280ca9ad697f2
hoandy033001 <aho37@usf.edu>
2026-09-04 20:03:47 +0000

    Added blank input/output files and setup basic information for cobol program

---
+++ InCollege.cob
@@ -0,0 +1,16 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. InCollege.
+       AUTHOR. Rafael Hernandez Vantuyl, Andy Ho, Steven Huynh, Joanna
+       Johnson, Lynberg Jean.
+       DATE-WRITTEN. September 4th, 2026.
+
+
+
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+           SELECT INPUT_FILE 
+           ASSIGN TO
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+           
