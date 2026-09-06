ed7d8ce401b7c983461eb796b656609d5ac2b186
hoandy033001 <aho37@usf.edu>
2026-09-06 05:01:21 +0000

    Added base login functionality to program and simulated with input file

--- InCollege.cob
+++ InCollege.cob
@@ -4,8 +4,6 @@
        AUTHOR. Joanna Johnson, Lynberg Jean.
        DATE-WRITTEN. September 4th, 2026.
 
-      
-
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
@@ -17,11 +15,50 @@
        FILE SECTION.
        FD INPUT-FILE.
            01 INPUT-RECORD.
-               05 USER-NAME PIC X(50).
+               05 USER-INPUT PIC X(50).
        WORKING-STORAGE SECTION.
+           01 EOF PIC X(1) VALUE 'N'.
        PROCEDURE DIVISION.
+           OPEN INPUT INPUT-FILE
            DISPLAY "Welcome to InCollege!".
            DISPLAY "Log In".
            DISPLAY "Create New Account.".
            DISPLAY "Enter your choice: " WITH NO ADVANCING.
-       STOP RUN.
+
+           READ INPUT-FILE.
+           IF USER-INPUT = "Log In"
+               DISPLAY USER-INPUT
+               PERFORM LOGIN
+
+           ELSE
+               IF USER-INPUT = "Create New Account"
+                   DISPLAY USER-INPUT
+                   PERFORM CREATE-ACCOUNT.
+       LOGIN.
+      *    This print statement is just for debugging
+           DISPLAY "LOGIN PART".
+           DISPLAY "Please enter your username: " WITH NO ADVANCING.
+           READ INPUT-FILE.
+           DISPLAY USER-INPUT.
+           DISPLAY "Please enter your password: " WITH NO ADVANCING.
+           READ INPUT-FILE.
+           DISPLAY USER-INPUT.
+      *    TODO Check if user exists
+           DISPLAY "Logged in Successfully!"
+      *    Transition into post login.
+           CLOSE INPUT-FILE.
+           STOP RUN.
+
+       CREATE-ACCOUNT.
+           DISPLAY "CREATE ACCOUNT PART".
+           DISPLAY "Please enter your username: " WITH NO ADVANCING.
+           READ INPUT-FILE.
+           DISPLAY USER-INPUT.
+      *    To do: password validation    
+           DISPLAY "Please enter your password: " WITH NO ADVANCING.
+           READ INPUT-FILE.
+           DISPLAY USER-INPUT.
+           DISPLAY "Account Created!"
+      *    Transition into post login.
+           CLOSE INPUT-FILE.
+           STOP RUN.
