8b475c155e1121ac18a6254b4544fc291d10d694
hoandy033001 <aho37@usf.edu>
2026-09-06 02:33:46 +0000

    Put in introductory text with setup for the input file

--- InCollege.cob
+++ InCollege.cob
@@ -1,16 +1,27 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. InCollege.
-       AUTHOR. Rafael Hernandez Vantuyl, Andy Ho, Steven Huynh, Joanna
-       Johnson, Lynberg Jean.
+       AUTHOR. Rafael Hernandez Vantuyl, Andy Ho, Steven Huynh.
+       AUTHOR. Joanna Johnson, Lynberg Jean.
        DATE-WRITTEN. September 4th, 2026.
 
-
+      
 
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
-           SELECT INPUT_FILE 
-           ASSIGN TO
-       DATA DIVISION.
-       WORKING-STORAGE SECTION.
+           SELECT INPUT-FILE
+           ASSIGN TO "InCollege-Input.txt"
+           ORGANIZATION IS LINE SEQUENTIAL.
            
+       DATA DIVISION.
+       FILE SECTION.
+       FD INPUT-FILE.
+           01 INPUT-RECORD.
+               05 USER-NAME PIC X(50).
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+           DISPLAY "Welcome to InCollege!".
+           DISPLAY "Log In".
+           DISPLAY "Create New Account.".
+           DISPLAY "Enter your choice: " WITH NO ADVANCING.
+       STOP RUN.
