a829059156e9d82571b7271c5019f4383f126b9a
hoandy033001 <aho37@usf.edu>
2026-09-07 05:32:18 +0000

    Started work on post login menu and Password validation (DOES NOT RUN ATM)

--- InCollege.cob
+++ InCollege.cob
@@ -23,11 +23,21 @@
                05 USER-INPUT PIC X(50).
        FD OUTPUT-FILE.
            01 OUTPUT-RECORD PIC X(80).
+
        WORKING-STORAGE SECTION.
            01 EOF     PIC X(1) VALUE 'N'.
            01 LOG-MSG PIC X(80) VALUE SPACES.
            01 STRING-MESSAGE PIC X(80) VALUE SPACES.
            01 USER-NAME PIC X(80).
+           01 IS-VALID PIC X VALUE 'N'.
+               88 PASSWORD-VALID VALUE 'Y'.
+               88 PASSWORD-INVALID VALUE 'N'.
+           01 PASSWORD_isEight PIC X(1) VALUE 'N'.
+           01 PASSWORD_notTwelve PIC X(1) VALUE 'N'.
+           01 PASSWORD_hasDigit PIC X(1) VALUE 'N'.
+           01 PASSWORD_hasNum PIC X(1) VALUE 'N'.
+           01 PASSWORD_hasSpec PIC X(1) VALUE 'N'.
+           01 PASSWORD_CHARACTER PIC X(1).
        PROCEDURE DIVISION.
            OPEN INPUT  INPUT-FILE
            OPEN OUTPUT OUTPUT-FILE
@@ -50,7 +60,6 @@
            ELSE
                IF USER-INPUT = "Create New Account"
                    PERFORM CREATE-ACCOUNT.
-
        LOGIN.
            READ INPUT-FILE
            STRING "Please enter your username: " DELIMITED BY SIZE
@@ -69,11 +78,10 @@
            MOVE STRING-MESSAGE TO LOG-MSG.
            PERFORM WRITE-OUTPUT.
            INITIALIZE STRING-MESSAGE.
+           PERFORM PASSWORD-VALIDATION.
            MOVE "Logged in Successfully!" TO LOG-MSG
            PERFORM WRITE-OUTPUT
            PERFORM POST-LOGIN.
-
-
        CREATE-ACCOUNT.
            READ INPUT-FILE
            STRING "Please enter your username: " DELIMITED BY SIZE
@@ -84,19 +92,11 @@
            PERFORM WRITE-OUTPUT.
            INITIALIZE STRING-MESSAGE.
            MOVE USER-INPUT TO USER-NAME.
-           READ INPUT-FILE
-           STRING "Please enter your password: " DELIMITED BY SIZE
-                   USER-INPUT DELIMITED BY SPACE 
-             INTO STRING-MESSAGE
-           END-STRING.
-           MOVE STRING-MESSAGE TO LOG-MSG.
-           PERFORM WRITE-OUTPUT.
-           INITIALIZE STRING-MESSAGE.
+           PERFORM PASSWORD-VALIDATION.
            MOVE "Account Created Successfully!" TO LOG-MSG
            PERFORM WRITE-OUTPUT
-           PERFORM POST-LOGIN.
-  
-           
+           PERFORM POST-LOGIN.         
+
        POST-LOGIN.
            STRING "Welcome, " DELIMITED BY SIZE
                    USER-NAME DELIMITED BY SPACE
@@ -106,14 +106,47 @@
            MOVE STRING-MESSAGE TO LOG-MSG.
            PERFORM WRITE-OUTPUT.
            INITIALIZE STRING-MESSAGE.
-
-
-
+           MOVE "1. Search for a job" TO LOG-MSG
+           PERFORM WRITE-OUTPUT.
+           MOVE "2. Find someone you know" TO LOG-MSG.
+           PERFORM WRITE-OUTPUT.
+           MOVE "3. Learn a new skill" TO LOG-MSG.
+           PERFORM WRITE-OUTPUT.
+           READ INPUT-FILE.
+           STRING "Enter your choice: " DELIMITED BY SIZE
+                   USER-INPUT DELIMITED BY SPACE
+               INTO STRING-MESSAGE
+           END-STRING.
+           PERFORM WRITE-OUTPUT.
+           INITIALIZE STRING-MESSAGE.
+      *    TO DO: MAKE ACTUAL MENU
            CLOSE INPUT-FILE.
            CLOSE OUTPUT-FILE.
            STOP RUN.
+       
+      
+       
+       PASSWORD-VALIDATION.
+           PERFORM UNTIL IS-VALID
+              READ INPUT-FILE.
+               MOVE 
+               STRING "Please enter your password: " DELIMITED BY SIZE
+                       USER-INPUT DELIMITED BY SPACE 
+               INTO STRING-MESSAGE
+               END-STRING.
+               MOVE STRING-MESSAGE TO LOG-MSG.
+               PERFORM WRITE-OUTPUT.
+               INITIALIZE STRING-MESSAGE.
+
+               IF USER-INPUT(8:1) NOT = SPACE
+                   MOVE 1 TO PASSWORD_isEight.
+               IF USER-INPUT(12:1) = SPACE.
+                   MOVE 1 TO PASSWORD_notTwelve.
+               
+               PERFORM VARYING
+                
       *    KAN-41/42: Dual output - display to console AND write to file
-      *    TRIM ensures both outputs are identical (no trailing spaces)
+      *    TRIM ensures both outputs are identical (no trailing spaces)                               
        WRITE-OUTPUT.
            MOVE FUNCTION TRIM(LOG-MSG TRAILING) TO OUTPUT-RECORD
            DISPLAY OUTPUT-RECORD
